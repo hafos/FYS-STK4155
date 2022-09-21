43313d70d57b416093badeeee5b56a9302596880
semya <semyaat@uio.no>
2022-09-21 17:54:37 +0200

    plots now look like something

--- project1/OLS.py
+++ project1/OLS.py
@@ -21,19 +21,41 @@ x, y = np.meshgrid(x,y)
 print(np.shape(x))
 
 z = FrankeFunction(x, y, noise=False)
+# z /= np.max(z)
+
+
 # Scaling by subtracting mean value 
 # Q : Skal man skalere x of y eller z ?
-# x = scale(x, axis = 1)
-# y = scale(y, axis = 0)
+x = scale(x, axis = 1)
+y = scale(y, axis = 0)
 
 def OLS(x, y, z, p=5): 
     X = DesignMatrix(x, y, p)
+
+    # print(X)
 
     # Split into training and testing 
     ratio = 0.2
     X_train, X_test, z_train, z_test = train_test_split(X, np.ravel(z), test_size=ratio)
 
-    return X_train, X_test, z_train, z_test 
+    # Scale the data ?? 
+    X_train = X_train - np.mean(X_train)
+    X_test = X_test - np.mean(X_test)
+
+    beta = get_beta(X_train, z_train)
+    z_tilde_train = X_train @ beta # fitted z 
+    z_tilde_test  = X_test  @ beta # predicted z 
+
+    # MSE 
+    train_MSE = MSE(z_train.ravel(), z_tilde_train.ravel())
+    test_MSE  = MSE(z_test.ravel(), z_tilde_test.ravel())
+    # R2
+    train_R2  = R2(z_train.ravel(), z_tilde_train.ravel())
+    test_R2   = R2(z_test.ravel(), z_tilde_test.ravel())
+
+    return X_train, X_test, z_train, z_test, train_MSE, test_MSE, train_R2, test_R2, beta
+
+OLS(x,y,z,p=5)
 
 # Plot the resulting scores (MSE and R²) as functions of the polynomial degree 
 
@@ -41,24 +63,23 @@ def plot_OLS(x,y,z, p):
     """ Plots the MSE and R² scores as functions of the polynomial degree 
     
     """
-    X = DesignMatrix(x, y, p)
-    X_train, X_test, z_train, z_test = OLS(x, y, z, p)
-    beta = get_beta(X_train, z_train)
-    z_tilde_train = X_train @ beta # predicted z 
-    z_tilde_test  = X_test  @ beta # predicted z 
+    # X = DesignMatrix(x, y, p)
+    
 
     # Create arrays 
-    train_MSE = np.zeros(p)
-    test_MSE  = np.zeros(p)
-    train_R2  = np.zeros(p)
-    test_R2   = np.zeros(p)
-    p_array = np.zeros(p)
+    train_MSE_arr = np.zeros(p)
+    test_MSE_arr  = np.zeros(p)
+    train_R2_arr  = np.zeros(p)
+    test_R2_arr   = np.zeros(p)
+    beta_list = []
+    p_array = np.linspace(1,p,p)
     i = 0
 
     for n in range(1, p+1):
+        X_train, X_test, z_train, z_test, train_MSE, test_MSE, train_R2, test_R2, beta = OLS(x, y, z, p=n)
         print(f"Order {n}")
-        z_all = X @ get_beta(X, z.ravel())
-        z_all = np.reshape(z_all, (np.shape(x)[0], np.shape(x)[0]))
+        # z_all = X @ get_beta(X, z.ravel())
+        # z_all = np.reshape(z_all, (np.shape(x)[0], np.shape(x)[0]))
         
         # fig, ax = plt.subplots(subplot_kw={'projection':'3d'})
         # Plot the surface.
@@ -72,18 +93,28 @@ def plot_OLS(x,y,z, p):
 
         # Add a color bar which maps values to colors.
         # fig.colorbar(surf, shrink=0.5, aspect=5)
-        train_MSE[i] = MSE(z_train.ravel(), z_tilde_train.ravel())
-        test_MSE[i]  = MSE(z_test.ravel(), z_tilde_test.ravel())
-        p_array[i] = n
-        i+=1
+        # train_MSE[i] = MSE(z_train.ravel(), z_tilde_train.ravel())
+        beta_list.append(beta)
+        train_MSE_arr[i] = train_MSE
+        test_MSE_arr[i]  = test_MSE
+        train_R2_arr[i] = train_R2
+        test_R2_arr[i]  = test_R2
+        # p_array[i] = n
+        i += 1
 
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    ax[0].plot(p_array, train_MSE_arr, label="Train MSE")
+    ax[0].plot(p_array, test_MSE_arr, label="Test MSE")
 
-    fig, ax = plt.subplots()
-    ax.plot(p_array, train_MSE, label="Train MSE")
-    ax.plot(p_array, test_MSE, label="Test MSE")
-    ax.set_yscale('log')
-    ax.legend()
+    ax[1].plot(p_array, train_R2_arr, label="Train R2")
+    ax[1].plot(p_array, test_R2_arr, label="Test R2")
+    # ax.plot(p_array, beta, label="beta")
+    ax[0].set_yscale('log')
+    ax[0].legend()
+    ax[1].legend()
+    fig.tight_layout()
     plt.show()
+
 
 p = 5
 plot_OLS(x,y,z, p)
--- project1/functions.py
+++ project1/functions.py
@@ -14,29 +14,27 @@ def DesignMatrix(x, y, p):
     """
 
     # Flatten array using ravel ? XXX
-    if len(x.shape) > 1: 
-        x = np.ravel(x)
-        y = np.ravel(y)
+    # if len(x.shape) > 1: 
+    x = np.ravel(x)
+    y = np.ravel(y)
 
     N = len(x)
-    a = int((p+1)*(p+2)/2) # Number of elements in beta 
+    a = int((p + 1)*(p + 2)/2) # Number of elements in beta 
     X = np.ones((N,a))
 
-    columns = [r'$x^0 y^0$']
+    j = 1 
+    # columns = [r'$x^0 y^0$']
     for i in range(1, p+1): 
         q = int(i + (i+1)/2)
         for k in range(i+1): 
-            X[:,q+k] = x**(i-k) * y**k 
-            columns.append(r'$x^{%i} y^{%i}$'%((i-k), k))
+            X[:,j] = x**(i-k) * y**k 
+            # X[:,q+k] = x**(i-k) * y**k # Got an out of range error here 
+            # columns.append(r'$x^{%i} y^{%i}$'%((i-k), k))
+            j += 1
+            # print("j", j)
+            # print("q", q+k)
 
     # print(columns) # Print x, y, x², xy, y², etc 
-
-    # Tried to get a nice print but i cant get it to work, not important 
-    # DesignMatrix = pd.DataFrame(X)
-    # DesignMatrix.index = x
-    # DesignMatrix.columns = ['1', 'A', 'A^(2/3)', 'A^(-1/3)', '1/A']
-    # pd.display(DesignMatrix)
-
     return X 
 
 def get_beta(X, y): 
@@ -46,10 +44,9 @@ def get_beta(X, y):
     # Train model 
 
     # b = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y) # Evt dette? 
-    # b = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
 
-    # b = np.linalg.pinv(X.T @ X) @ X.T @ y
-    b = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(y)
+    b = np.linalg.pinv(X.T @ X) @ X.T @ y.ravel()
+    # b = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(y)
 
     return b
 
