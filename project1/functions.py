71606c81d351467b5808cd4b5b0d19b7baafbe86
semya <semyaat@uio.no>
2022-10-10 15:15:26 +0200

    added functions

--- project1/functions.py
+++ project1/functions.py
@@ -15,6 +15,11 @@ def R2(func, func_app):
     R_two+=1  
     return R_two 
 
+def get_MSE(y, y_tilde): 
+    mse = 1/np.size(y) * np.sum((y - y_tilde)**2)
+    n = np.size(y)
+    # 1/len(z_test)*np.sum(np.power(z_test - ytilde_test, 2))
+    return np.sum((y-y_tilde)**2)/n
 
 def FrankeFunction(x,y,sigma):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
@@ -24,3 +29,5 @@ def FrankeFunction(x,y,sigma):
     values = term1 + term2 + term3 + term4 
     values = np.concatenate(values,axis=None) + sigma*np.random.randn(len(x)*len(y))
     return values
+
+
